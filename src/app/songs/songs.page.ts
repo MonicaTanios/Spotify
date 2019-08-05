31ff7770d6f236aa98f7bbb2332b086166b9e1d8
Monica Tanios <monicatanios@gmail.com>
2019-08-05 13:44:15 +0200

    SpotifyService Trial.

--- src/app/songs/songs.page.ts
+++ src/app/songs/songs.page.ts
@@ -1,16 +1,28 @@
+import { SpotifyService } from './../services/spotify.service';
+import { TestService } from './../test.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-songs',
   templateUrl: './songs.page.html',
-  styleUrls: ['./songs.page.scss'],
+  styleUrls: ['./songs.page.scss']
 })
 export class SongsPage implements OnInit {
 
-  constructor(private _route: ActivatedRoute) { }
+  tracksList: any;
+  data: any = {};
+
+  constructor( private spotifyService: SpotifyService,
+    private _route: ActivatedRoute) {
+  }
 
   ngOnInit() {
+   let temp =  this.spotifyService.getTracksStatic()
+   console.log(temp);
+   
+    this.data = JSON.parse(JSON.stringify(this.spotifyService.getTracksStatic()));
   }
 
 }
