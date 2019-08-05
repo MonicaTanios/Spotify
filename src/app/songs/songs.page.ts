f0393477083cab8c4d6b1fb37763b7a68433ee27
Monica Tanios <monicatanios@gmail.com>
2019-08-05 12:38:06 +0200

    Add Songs page.

---
+++ src/app/songs/songs.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-songs',
+  templateUrl: './songs.page.html',
+  styleUrls: ['./songs.page.scss'],
+})
+export class SongsPage implements OnInit {
+
+  constructor(private _route: ActivatedRoute) { }
+
+  ngOnInit() {
+  }
+
+}
